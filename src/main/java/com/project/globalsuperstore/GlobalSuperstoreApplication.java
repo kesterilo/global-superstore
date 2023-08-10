fce410074b1b119388fb2bf3adbb0ddbd399b33e
Kester <ilokester@gmail.com>
2023-08-10 11:18:27 +0100

    project's pilot edition

---
+++ src/main/java/com/project/globalsuperstore/GlobalSuperstoreApplication.java
@@ -0,0 +1,13 @@
+package com.project.globalsuperstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalSuperstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalSuperstoreApplication.class, args);
+	}
+
+}
